2f262b5447b0a20644f45b2c5b8c6d58dace7acd
Emrul Chowdhury <emrul.cse@metrouni.edu.bd>
2017-05-02 23:25:57 +0600

    Create cf-define.cpp

---
+++ cf-define.cpp
@@ -0,0 +1,67 @@
+// Bismillahir Rahmanir Rahim
+// Mr_Emrul
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef vector <int> vi;
+typedef vector <ll> vl;
+typedef pair <ll, ll> pll;
+typedef pair <ll, pll> plp;
+typedef pair <int, int> pii;
+typedef pair <int, pii> pip;
+typedef vector < pii > vii;
+typedef vector < pll > vll;
+typedef map <string, ll> mpsl;
+typedef map <string, int> mpsi;
+typedef map <int, int> mii;
+typedef map <ll, ll> mll;
+typedef map <pll, ll> mpll;
+const ll MX = 2000005;
+const ll mod = 1e7 + 10;
+
+#define LCM(a,b)        (a / __gcd(a,b) ) *-b
+#define gcd(a,b)        __gcd(a,b)
+#define all(x)          (x).begin(), (x).end()
+#define mem(a, n)       memset(a,n,sizeof(a))
+#define for1(i, n)      for(i=1; i<=n; i++)
+#define for0(i, n)      for(i=0; i<n; i++)
+#define rof0(i, n)      for(i=n-1; i>=0; i--)
+#define rof1(i, n)      for(i=n; i>=1; i--)
+#define forab(i, a, b)  for(i=a; i<=b; i++)
+#define rofab(i, a, b)  for(i=b; i>=a; i--)
+#define pb              push_back
+#define pbb             pop_back
+#define YES             cout << "YES" << endl
+#define NO              cout << "NO" << endl
+#define sin(s)          getline(cin, s)
+#define scf(a)          cin >> a
+#define scf2(a, b)      cin >> a >> b
+#define scf3(a, b, c)   cin >> a >> b >> c
+#define scf4(a,b,c,d)   cin >> a >> b >> c >> d
+#define pnf(a)          cout << (a) << endl
+#define pnf2(a, b)      cout << (a) << " " << (b) << endl
+#define pnf3(a, b, c)   cout << (a) << " " << (b) << " " << (c) << endl
+#define pnfa(a)         cout << (a) << " "
+#define pf(a)           cout << (a)
+#define sp              cout << " "
+#define nl              cout << endl
+#define sz(n)           n.size()
+#define clr(v)          v.clear()
+#define bug(n)          cout << ">> " << n << " <<" << endl
+#define min3(a, b, c)   min(a, min(b, c))
+#define max3(a, b, c)   max(a, max(b, c))
+#define in              freopen("in.txt", "r", stdin)
+#define out             freopen("out.txt", "w", stdout)
+#define fast            { ios_base::sync_with_stdio(false); cin.tie(0); }
+#define F               first
+#define S               second
+#define mpp             make_pair
+
+int main()
+{
+    fast;
+    ll n, t, q, i, j, cnt=0, sum=0, mx=0, mn=1e18;
+    
+}
